11ccd29888c3f59c420ed217386882cc80c61f33
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2018-12-30 23:43:01 -0500

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+// webpack v4
+const path = require('path');
+// update from 23.12.2018
+const nodeExternals = require('webpack-node-externals');
+
+
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  target: 'node', // update from 23.12.2018
+  externals: [nodeExternals()], // update from 23.12.2018
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          'postcss-loader',
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new HtmlWebpackPlugin({
+        inject: false,
+        hash: true,
+        template: './src/index.html',
+        filename: 'index.html'
+      })
+  ]
+};
